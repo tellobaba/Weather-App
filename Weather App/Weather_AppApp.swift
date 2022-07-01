78f0e673cdb6503d03423ea8385ccf0dd8f31958
Oluwatomiwa <olatunjitello17@gmail.com>
2022-07-01 12:22:02 +0100

    Initial Commit

---
+++ Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Oluwatomiwa Olatunji on 01/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
